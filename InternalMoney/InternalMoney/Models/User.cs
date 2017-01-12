8fb27e45e401cce9d6e15dc14cf80b579a50a003
sergey-krokhmal <xai.sergey.krokhmal@gmail.com>
2017-01-12 22:15:56 +0300

    Added User model for EF

---
+++ InternalMoney/InternalMoney/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InternalMoney.Models
+{
+	public class User
+	{
+		public int Id { get; set; }
+
+		[Required]
+		public string Name { get; set; }
+
+		[Required]
+		public string Email { get; set; }
+
+		[Required]
+		public string Password { get; set; }
+	}
+}

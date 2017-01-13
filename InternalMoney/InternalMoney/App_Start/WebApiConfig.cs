caab6a91a8ecff1254e3ad43327640664d88789c
sergey-krokhmal <xai.sergey.krokhmal@gmail.com>
2017-01-14 02:10:03 +0300

    Transaction model created

--- InternalMoney/InternalMoney/App_Start/WebApiConfig.cs
+++ InternalMoney/InternalMoney/App_Start/WebApiConfig.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Web.Http;
 
 namespace InternalMoney
@@ -10,6 +11,7 @@ namespace InternalMoney
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+			config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("application/octet-stream"));
 
             // Web API routes
             config.MapHttpAttributeRoutes();
---
+++ InternalMoney/InternalMoney/Models/Transaction.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InternalMoney.Models
+{
+	public class Transaction
+	{
+		public int Id { get; set; }
+
+		[Required]
+		public int Id_Sender { get; set; }
+
+		public User Sender { get; set; }
+
+		[Required]
+		public int Id_Receiver { get; set; }
+
+		public User Receiver { get; set; }
+
+		[Required]
+		public decimal Money { get; set; }
+	}
+}

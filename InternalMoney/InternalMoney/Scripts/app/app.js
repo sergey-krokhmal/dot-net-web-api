64ac4e11a70c7978cf432ef4996e21247f56e40a
sergey-krokhmal <xai.sergey.krokhmal@gmail.com>
2017-01-14 17:07:43 +0300

    Create test angular

--- InternalMoney/InternalMoney/Scripts/app/app.js
+++ InternalMoney/InternalMoney/Scripts/app/app.js
@@ -1,1 +1,8 @@
-﻿
+﻿var questApp = angular.module('internalMoneyApp', ["ngRoute"])
+    .config(function ($routeProvider) {
+        $routeProvider.when('/test',
+        {
+            templateUrl: 'Views/test.html',
+            controller: 'TestController'
+        });
+    });
